9ba977db3db06fb770d9ab104b07d5e65247cfd9
sozooo <110661290+sozooo@users.noreply.github.com>
2023-11-02 11:51:29 +0300

    Update VolumeScript.cs

--- Assets/Scripts/VolumeScript.cs
+++ Assets/Scripts/VolumeScript.cs
@@ -8,22 +8,14 @@ public class VolumeScript : MonoBehaviour
     [SerializeField] private GameObject _target;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _radius;
-    [SerializeField] private float _recoveryRate;
     [SerializeField] private float _maxVolume;
 
-    private float _currentModificator;
     private float _minVolume = 0f;
-
-    private void Awake()
-    {
-        _currentModificator = _radius;
-    }
 
     private void Update()
     {
         Vector2 distance = _target.transform.position - transform.position;
-        _currentModificator = Mathf.MoveTowards(_currentModificator, distance.magnitude, _recoveryRate * Time.deltaTime);
 
-        _audioSource.volume = Mathf.Lerp(_maxVolume, _minVolume, _currentModificator / _radius);
+        _audioSource.volume = Mathf.Lerp(_maxVolume, _minVolume, distance.magnitude / _radius);
     }
 }
